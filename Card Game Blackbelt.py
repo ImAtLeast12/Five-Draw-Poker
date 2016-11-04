b93edaf4bdf6d8e209e6b17432ae124a20c41328
ImAtLeast12 <imatleast12@gmail.com>
2016-11-04 04:59:37 -0400

    This made things cleaner
    I don't know what the difference is but this is what I have

--- Card Game Blackbelt.py
+++ Card Game Blackbelt.py
@@ -16,10 +16,6 @@ rankName = ("Ace", "Two", "Three", "Four", "Five", "Six", "Seven",
 playerName = ("deck", "player", "computer")
 
 
-def showDeck():
-    for i in range(NUMCARDS):
-        print(cardLoc[i])
-        
 def clearDeck():
     print("Location of all cards")
     print("#" + "\t" + "card" + "\t" + "\t " + "Location")
@@ -39,6 +35,7 @@ def clearDeck():
             tabs = countTabs * "\t"
                 
             cardLoc[cardIndex] = (str(cardIndex)) + "\t" +  rankName[x] + " of " + suitName[y] + tabs + "deck"
+
             
 def assignCard(playerIndex):
     keepGoing = True
@@ -50,12 +47,23 @@ def assignCard(playerIndex):
             cardLoc[i] = cardLoc[i][:(-1* (playerName[0].__len__()))]
             cardLoc[i] += playerName[playerIndex] 
             keepGoing = False
+        
+def showDeck():
+    for i in range(NUMCARDS):
+        print(cardLoc[i])
  
 def showHand(playerIndex):
+    hand = []
     for i in range(NUMCARDS): 
         if (cardLoc[i][(-1 * (playerName[playerIndex].__len__())):] == playerName[playerIndex]):
             print(cardLoc[i])
             
+            if (cardLoc[i][1:2] == "\t"):
+                hand.append(i) #= cardLoc[i][:1]#either is one digit 
+            else:
+                hand.append(i) #= cardLoc[i][:2]#or 2 digits
+    return hand
+
 def main(): #What I want is to make a card game
             #however what cardgame should I make
             #Poker (hard), 21 mabey, go fish, 
@@ -73,174 +81,194 @@ def main(): #What I want is to make a card game
     #Then scoring the cards
         #see who beat who
 
-def score(playerIndex): #This needs the input of whos hand you are on
-    #Sort the cards from greatest value to least A,K,Q,J,10, ... 3,2
-    #to do this we will have to look at it's rankName
-    hand = [0] * 5
+def Score(playerIndex): #This needs the input of whos hand you are on
+
+    Sort = []
+    i = 1
+    for i in range(len(playerName)):
+        hand = showHand(i) #this stores the hand you are working on for which ever player you are on
+        Sort.append(Sort(hand))
     
-    j = 0 #this is just the index for hand
-    for i in range(NUMCARDS):
-        if (cardLoc[i][(-1 * (playerName[1].__len__())):] == playerName[1]):
-            hand[j] = i #this will assign the indexes of the cardlocation into hand
-            j = +1 
+    Scoring = [HighCard(hand), OnePair(hand), TwoPair(hand),ThreeOfAKind(hand), Straight(hand),Flush(hand),FullHouse(hand),FourOfAKind(hand),StraightFlush(hand),RoyalFlush(hand)]
+    return Scoring
 
-    #Hand = [6,16,31,49,50]
-            #stands for 7H, 4D, 6C, JC, QC
+    
+def Sort(hand):
+    #hand = [2,16,31,49,50]
+    #look at card Location and get rid of the "index, of suit\t\tplayer"
+    Arr = [-1] * 5
+    Ar2 = [-1] * 5
+    
+    for y in range (len(hand)): #
+        for x in range (len(rankName)): #
+            if (cardLoc[hand[y]].find(rankName[x]) != -1): # If it finds a match        
+                if (x == 0): #
+                    x = 13   #
+                Arr[y] = x + 1  #All of this works I think I just need to sort it from least to greatest
+    Arr.sort()
 
-    #Now I should Sort them
-            #should be 4D, 6C, 7H, JC, QC
-                #or Just 4,6,7,J,Q
+    for y in range (len(hand)):
+        for x in range (len(suitName)):
+            if (cardLoc[hand[y]].find(suitName[x]) != -1):
+                Ar2[y] = x #this will tell me what suit they are
 
-    #the High Hand is = Q or hand[4]
+    #I need to do the same thing but for Hearts, Diamonds, Spades, and Clubs
+                
+    return Arr, Ar2 #This gives me my precious Arr, Ar2 So that I can use them in the Scoring
 
-
-
-
-
-
-    #FOR TESTING PURPOSES I CAN SET HAND = [0,1,2,3,4] IF I WANT TO TEST THE FUNCTIONALITY
+def HighCard (Arr,Ar2):
+    
+    #hand = [2,16,31,49,50]                             BEFORE
+    #Arr = ["Three", "Four", "Six", "Jack", "Queen"]    
+    #What I want is = [3,4,6,11,12]                     AFTER
+    HighCard = Arr[4]
+    return HighCard
             
+                
 
+    #scoring = [-1] * 10 #everything = -1 in scoring 2d array
+    #scoring = [scoring] * (len(playerName) - 1) #this will make a 2d list of length 9 for (High, 1 Pair, 2 Pair, 3 of a Kind, Straight, Flush, Full house, Four of a Kind, Straight Flush, Royal Flush)
+
+    #scoring[0] = HighCard #Scoring = [12, -1, -1, -1, -1, -1, -1, -1, -1, -1]
+    #I should have a tuple for all of the hand combinations for what ever player I am on scoring[0] = [Q,0,0,0,0,0,0,0,0,0]
+    #So when I am done the winner will be the one that has the highest 1
 
     
+
+
+#1 PAIR
+def OnePair (hand,Arr,Ar2):
+    #USE Arr[] because it is ordered from greatest to least
+    OnePair = 0
+    for i in range (4):
+        if (Arr[0 + i] == Arr[1 + i]):
+            OnePair = Arr[0 + i]
+            
+    return OnePair #This will tell if there is a pair and what kind of pair it was
     
-    #HIGH HAND above
-            #I want to sort my cards from highest to lowest
-    def Score(hand):
-        #hand = [2,16,31,49,50]
-        #look at card Location and get rid of the "index, of suit\t\tplayer"
-        Arr = [-1] * 5
-        Ar2 = [-1] * 5
-        
+    
+#2 PAIR
+def TwoPair (hand,Arr,Ar2):
+    TwoPair = 0
+    if Arr[0] == Arr[1]:
+        if Arr[2] == Arr[3]:
+            TwoPair = Arr[3]
+        if Arr[3] == Arr[4]:
+            TwoPair = Arr[4]
+    if Arr[1] == Arr[2]:
+        if Arr[3] == Arr[4]:
+            TwoPair = Arr[4]
+    return TwoPair
 
-        for y in range (len(hand)): #this will be the index for the CardLoc[y]
-            for x in range (len(rankName)): # this will be the index for rankName[x]
-                if (cardLoc[hand[y]].find(rankName[x]) != -1):
-                    if (x == 0): #if the card is Ace
-                        x = 13   #Ace is the best so make it 13
-                    Arr[y] = x + 1  #Add another 1 so that "Three" = 3
+    
+#3 OF A KIND
+def ThreeOfAKind (hand,Arr,Ar2):
+    ThreeOfAKind = 0
+    for i in range(3):
+        if (Arr[0 + i] == Arr[1 + i]):
+            if (Arr[1 + i] == Arr[2 + i]):
+                if (Arr[2 + i] == Arr [3 + i]):                   
+                    ThreeOfAKind = Arr[0 + i]
+    return ThreeOfAKind
 
-        for y in range (len(hand)):
-            for x in range (len(suitName)):
-                if (cardLoc[hand[y]].find(suitName[x]) != -1):
-                    Ar2[y] = x #this will tell me what suit they are
+#STRAIGHT
+def Straight (hand,Arr,Ar2):
+    Straight = 0
+    if (Arr[0] + 1== Arr[1]):
+        if (Arr[1] + 1== Arr[2]):
+            if (Arr[2] + 1 == Arr[3]):
+                if (Arr[3] + 1 == Arr[4]):
+                    #                             [         ]         [         ]
+                    #this only works if it is from 0,1,2,3,4,5,6,7,8,9,10,J,Q,K,A
+                    Straight = Arr[4]
+    
 
-        #I need to do the same thing but for Hearts, Diamonds, Spades, and Clubs
-        
-
-
-
-
-
-        
-
-        #hand = [2,16,31,49,50]                             BEFORE
-        #Arr = ["Three", "Four", "Six", "Jack", "Queen"]    
-        #What I want is = [3,4,6,11,12]                     AFTER
-        HighCard = Arr[4]
-        
+    #This works for the others that wouldn't
+    if(Arr[0] == 2):
+        if(Arr[1] == 11): 
+            if(Arr[2] == 12): 
+                if(Arr[3] == 13): 
+                    if(Arr[4] == 14): 
+                        print("good 2 high")
+                        Straight = 2
+                        
+        if(Arr[1] == 3): 
+            if(Arr[2] == 12):
+                if(Arr[3] == 13):
+                    if(Arr[4] == 14): 
+                        print("good 3 high")
+                        Straight = 3
+            if(Arr[2] == 4):
+                if(Arr[3] == 13): 
+                    if(Arr[4] == 14): 
+                        print("good 4 high")
+                        Straight = 4
+                if(Arr[3] == 5):
+                    if(Arr[4] == 14): 
+                        print("good 5 high")
+                        Straight = 5
+    return Straight
                 
-                    
+    
+    #I need to make sure that these are what it is equal to 
+    #[ 2,11,12,13,14]
+    #[ 2, 3,12,13,14]
+    #[ 2, 3, 4,13,14]
+    #[ 2, 3, 4, 5,14]
 
-        scoring = [-1] * 10 #everything = -1 in scoring 2d array
-        #scoring = [scoring] * (len(playerName) - 1) #this will make a 2d list of length 9 for (High, 1 Pair, 2 Pair, 3 of a Kind, Straight, Flush, Full house, Four of a Kind, Straight Flush, Royal Flush)
+    
+#FLUSH
+def Flush (hand,Arr,Ar2):
+    flush = False
+    if (Ar2[0] == Ar2[1]):
+        if (Ar2[1] == Ar2[2]):
+            if (Ar2[2] == Ar2[3]):
+                if (Ar2[3] == Ar2[4]):
+                    flush = True
+    return flush
+    
+#FULL HOUSE
+def FullHouse(hand,Arr,Ar2):
+    FullHouse = 0
+    if Arr[0] == Arr[1]:
+        if Arr[1] == Arr[2]:
+            if Arr[3] == Arr[4]:
+                FullHouse = Arr[0]
+        if Arr[2] == Arr[3]:
+            if Arr[3] == Arr[4]:
+                FullHouse = Arr[4]
+    return FullHouse
+    
+#FOUR OF A KIND
+def FourOfAKind (hand):
+    FourOfAKind = 0
+    for i in range(2):
+        if (Arr[0 + i] == Arr[1 + i]):
+            if (Arr[1 + i] == Arr[2 + i]):
+                if (Arr[2 + i] == Arr[3 + i]):
+                    FourOfAKind = Arr[0 + i]
+    return FourOfAKind
 
-        scoring[0] = HighCard #Scoring = [12, -1, -1, -1, -1, -1, -1, -1, -1, -1]
-        #I should have a tuple for all of the hand combinations for what ever player I am on scoring[0] = [Q,0,0,0,0,0,0,0,0,0]
-        #So when I am done the winner will be the one that has the highest 1
+#STRAIGH FLUSH
+def StraightFlush (hand):
+    StraightFlush = False
+    if Straight(hand):
+        if Flush(hand):
+            StraightFlush = True
+    return StraightFlush
+    
+    #return (Straight(hand) and Flush(hand))
+    
+#ROYAL FLUSH
+def RoyalFlush (hand):
+    RoyalFlush = False
+    if (StraightFlush(hand)):
+            if (HighCard(hand) == 14):
+                RoyalFlush = True
+    return RoyalFlush
+    #return (StraightFlush(hand) and HighCard == 14)        #this will return true if hand was Straight, Flush, and HighCard
+                                                            #Else it will return False
 
-        
 
 
-    #1 PAIR
-        #USE Arr[] because it is ordered from greatest to least
-        Pair1 = 0
-        for i in range (5):
-            if (Arr[0 + i] == Arr[1 + i]):
-                Pair1 = Arr[0 + i]
-                
-        scoring[1] = Pair1 #This will tell if there is a pair and what kind of pair it was
-        
-        
-    #2 PAIR
 
-        
-    #3 OF A KIND
-
-        Pair3 = 0
-        for i in range(3):
-            if (Arr[0 + i] == Arr[1 + i]):
-                if (Arr[1 + i] == Arr[2 + i]):
-                    if (Arr[2 + i] == Arr [3 + i]):                   
-                        Pair3 = Arr[0 + i]
-        scoring[3] = Pair3
-
-        
-    #STRAIGHT
-        straight = 0
-        if (Arr[0] + 1== Arr[1]):
-            if (Arr[1] + 1== Arr[2]):
-                if (Arr[2] + 1 == Arr[3]):
-                    if (Arr[3] + 1 == Arr[4]):
-                        #this only works if it is from 0,1,2,3,4
-                        straight = Arr[4]
-        
-
-        #This works for the others that wouldn't
-        if(Arr[0] == 2):
-            if(Arr[1] == 11): 
-                if(Arr[2] == 12): 
-                    if(Arr[3] == 13): 
-                        if(Arr[4] == 14): 
-                            print("good 2 high")
-                            straight = 2
-                            
-            if(Arr[1] == 3): 
-                if(Arr[2] == 12):
-                    if(Arr[3] == 13):
-                        if(Arr[4] == 14): 
-                            print("good 3 high")
-                            straight = 3
-                if(Arr[2] == 4):
-                    if(Arr[3] == 13): 
-                        if(Arr[4] == 14): 
-                            print("good 4 high")
-                            straight = 4
-                    if(Arr[3] == 5):
-                        if(Arr[4] == 14): 
-                            print("good 5 high")
-                            straight = 5
-        scoring[4] = straight
-                    
-        
-        #I need to make sure that these are what it is equal to 
-        #[ 2,11,12,13,14]
-        #[ 2, 3,12,13,14]
-        #[ 2, 3, 4,13,14]
-        #[ 2, 3, 4, 5,14]
-
-        
-    #FLUSH
-        flush = 0
-        if (Ar2[0] == Ar2[1])
-            if (Ar2[1] == Ar2[2])
-                if (Ar2[2] == Ar2[3])
-                    if (Ar2[3] == Ar2[4])
-                        flush = Ar2[0] + 1
-        scoring[5] = flush
-        
-    #FULL HOUSE
-    #FOUR OF A KIND
-
-        Pair4 = 0
-        for i in range(2):
-            if (Arr[0 + i] == Arr[1 + i]):
-                if (Arr[1 + i] == Arr[2 + i]):
-                    if (Arr[2 + i] == Arr[3 + i]):
-                        Pair4 = Arr[0 + i]
-        scoring[2] = Pair4 #if there is 4 all of the same value then this is equal to one of the cards
-    #STRAIGH FLUSH
-    #ROYAL FLUSH
-        if (flush != 0 or -1)
-            if (straight != 0 or -1)
-    return -1
